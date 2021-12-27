2a506a996a94251797b562206052feb8fad26e4f
Jaco Baldrich <jaco.baldrich@letgo.com>
2021-12-27 13:05:35 +0100

    first commit

---
+++ src/main/java/com/letgo/javaskeleton/JavaSkeletonApplication.java
@@ -0,0 +1,13 @@
+package com.letgo.javaskeleton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSkeletonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSkeletonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/letgo/javaskeleton/JavaSkeletonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.letgo.javaskeleton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSkeletonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
