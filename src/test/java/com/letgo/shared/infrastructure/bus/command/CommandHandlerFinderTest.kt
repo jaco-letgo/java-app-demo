f4da328608bd54cb4eedc860a51f536d320470b7
Jaco Baldrich <jaco.baldrich@letgo.com>
2022-02-17 14:11:51 +0100

    Add command handler finder test

---
+++ src/test/java/com/letgo/shared/infrastructure/bus/command/CommandHandlerFinderTest.kt
@@ -0,0 +1,41 @@
+package com.letgo.shared.infrastructure.bus.command
+
+import com.letgo.shared.application.bus.command.Command
+import com.letgo.shared.application.bus.command.CommandHandler
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+private class CommandHandlerFinderTest {
+    @Test
+    fun `It should return the correct handler for a given command`() {
+        val expectedHandler = BarCommandHandler()
+        val finder = CommandHandlerFinder(listOf(
+            FooCommandHandler(),
+            expectedHandler
+        ))
+
+        assertEquals(expectedHandler, finder.forCommand(BarCommand()))
+    }
+
+    @Test
+    fun `It should throw an exception when there is no handler for the given Command object`() {
+        val command = BarCommand()
+        val finder = CommandHandlerFinder(listOf(FooCommandHandler()))
+
+        assertThrows<Exception> { finder.forCommand(command) }.run {
+            assertEquals("No handler found for ${command::class}", message)
+        }
+    }
+
+    private class FooCommand : Command
+    private class BarCommand : Command
+
+    private class FooCommandHandler : CommandHandler<FooCommand> {
+        override fun handle(command: FooCommand) {}
+    }
+
+    private class BarCommandHandler : CommandHandler<BarCommand> {
+        override fun handle(command: BarCommand) {}
+    }
+}
--- src/test/java/com/letgo/shared/infrastructure/bus/command/InMemorySyncCommandBusTest.kt
+++ src/test/java/com/letgo/shared/infrastructure/bus/command/InMemorySyncCommandBusTest.kt
@@ -1,66 +1,17 @@
 package com.letgo.shared.infrastructure.bus.command
 
-import com.letgo.shared.application.bus.command.Command
-import com.letgo.shared.application.bus.command.CommandHandler
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 
 private class InMemorySyncCommandBusTest {
     @Test
     fun `It should dispatch Command objects`() {
-        val command = BarCommand("olakease")
-        val expectedHandlerToBeCalled = BarCommandHandler(command)
-        val commandBus = InMemorySyncCommandBus(
-            CommandHandlerFinder(
-                listOf(
-                    FooCommandHandler(command),
-                    expectedHandlerToBeCalled
-                )
-            )
-        )
+        val command = ACommand("olakease")
+        val handler = SpyCommandHandler(command)
+        val commandBus = InMemorySyncCommandBus(CommandHandlerFinder(listOf(handler)))
 
         commandBus.dispatch(command)
 
-        assertTrue(expectedHandlerToBeCalled.hasBeenCalled)
-    }
-
-    @Test
-    fun `It should throw an exception when there is no handler for the given Command object`() {
-        val command = BarCommand("olakease")
-        val commandBus = InMemorySyncCommandBus(
-            CommandHandlerFinder(
-                listOf(
-                    FooCommandHandler(command)
-                )
-            )
-        )
-
-        assertThrows<Exception> { commandBus.dispatch(command) }.run {
-            assertEquals("No handler found for ${command::class}", message)
-        }
-    }
-
-    private data class FooCommand(private val property: String) : Command
-    private data class BarCommand(private val property: String) : Command
-
-    private class FooCommandHandler(
-        private val expectedCommand: Command,
-        var hasBeenCalled: Boolean = false
-    ) : CommandHandler<FooCommand> {
-        override fun handle(command: FooCommand) {
-            hasBeenCalled = true
-            assertEquals(expectedCommand, command)
-        }
-    }
-
-    private class BarCommandHandler(
-        private val expectedCommand: Command,
-        var hasBeenCalled: Boolean = false
-    ) : CommandHandler<BarCommand> {
-        override fun handle(command: BarCommand) {
-            hasBeenCalled = true
-            assertEquals(expectedCommand, command)
-        }
+        assertTrue(handler.hasBeenCalled)
     }
 }
