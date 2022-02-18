21437169bab15402a40076d2d6da5a395db95562
Jaco Baldrich <jaco.baldrich@letgo.com>
2022-02-18 14:55:24 +0100

    Refactor InMemorySyncDomainEventPublisher

--- src/main/java/com/letgo/shared/infrastructure/event/publisher/InMemorySyncDomainEventPublisher.kt
+++ src/main/java/com/letgo/shared/infrastructure/event/publisher/InMemorySyncDomainEventPublisher.kt
@@ -3,27 +3,15 @@ package com.letgo.shared.infrastructure.event.publisher
 import com.letgo.shared.application.event.DomainEventPublisher
 import com.letgo.shared.application.event.DomainEventSubscriber
 import com.letgo.shared.domain.DomainEvent
-import java.util.function.Consumer
 
 class InMemorySyncDomainEventPublisher(
-    private val subscribers: List<DomainEventSubscriber>
+    private val subscribers: List<DomainEventSubscriber>,
 ) : DomainEventPublisher {
     override fun publish(events: List<DomainEvent>) {
-        events.forEach(Consumer { event: DomainEvent ->
-            subscribers.forEach(Consumer { subscriber: DomainEventSubscriber ->
-                tryToConsumeEvent(event, subscriber)
-            })
-        })
+        events.forEach { event -> subscribers.forEach { subscriber -> tryToConsumeEvent(event, subscriber) } }
     }
 
     private fun tryToConsumeEvent(event: DomainEvent, subscriber: DomainEventSubscriber) {
-        if (subscriber.isSubscribedTo(event)) {
-            try {
-                subscriber.consume(event)
-            } catch (throwable: Throwable) {
-                throwable.printStackTrace()
-                throw throwable
-            }
-        }
+        if (subscriber.isSubscribedTo(event)) subscriber.consume(event)
     }
 }
