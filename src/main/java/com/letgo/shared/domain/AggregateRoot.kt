9546315cd6fbfd4f208a7026d6644366fa55f337
Jaco Baldrich <jaco.baldrich@letgo.com>
2022-02-21 14:52:29 +0100

    Fix scope functions magic

--- src/main/java/com/letgo/shared/domain/AggregateRoot.kt
+++ src/main/java/com/letgo/shared/domain/AggregateRoot.kt
@@ -4,7 +4,7 @@ package com.letgo.shared.domain
 abstract class AggregateRoot {
     private val events: MutableList<DomainEvent> = mutableListOf()
 
-    fun retrieveEvents(): List<DomainEvent> = events.apply { clear() }.run { toList() }
+    fun retrieveEvents(): List<DomainEvent> = events.toList().also { events.clear() }
 
     protected fun storeEvent(event: DomainEvent) {
         events.add(event)
