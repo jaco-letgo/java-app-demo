2cc4ae9fa2017bd3b58a36bf25597e9e25c91cdd
Jaco Baldrich <jaco.baldrich@letgo.com>
2022-03-15 15:54:35 +0100

    Avoid breaking open close principle with reflections

--- src/main/java/com/letgo/book/infrastructure/bus/serialize/InMemoryCommandClassFinder.kt
+++ src/main/java/com/letgo/book/infrastructure/bus/serialize/InMemoryCommandClassFinder.kt
@@ -1,19 +1,16 @@
 package com.letgo.book.infrastructure.bus.serialize
 
-import com.letgo.book.application.changeTitle.ChangeTitleCommand
-import com.letgo.book.application.create.CreateBookCommand
 import com.letgo.shared.application.bus.command.Command
 import com.letgo.shared.infrastructure.InfrastructureService
 import com.letgo.shared.infrastructure.bus.serialize.CommandClassFinder
+import org.reflections.Reflections
 import kotlin.reflect.KClass
 
 @InfrastructureService
 object InMemoryCommandClassFinder : CommandClassFinder {
-    private val mapping: Map<String, KClass<out Command>> = listOf(
-        CreateBookCommand::class,
-        ChangeTitleCommand::class,
-    ).associateBy { it.simpleName.toString() }
+    private val mapping: Map<String, Class<out Command>> =
+        Reflections("com.letgo.book").getSubTypesOf(Command::class.java).associateBy { it.simpleName.toString() }
 
     override fun find(type: String): KClass<out Command> =
-        mapping[type] ?: throw NoSuchElementException("No mapping for command with type $type")
+        mapping[type]?.kotlin ?: throw NoSuchElementException("No mapping for command with type $type")
 }
--- src/main/java/com/letgo/book/infrastructure/bus/serialize/InMemoryDomainEventClassFinder.kt
+++ src/main/java/com/letgo/book/infrastructure/bus/serialize/InMemoryDomainEventClassFinder.kt
@@ -1,19 +1,16 @@
 package com.letgo.book.infrastructure.bus.serialize
 
-import com.letgo.book.domain.BookCreated
-import com.letgo.book.domain.BookTitleChanged
 import com.letgo.shared.domain.DomainEvent
 import com.letgo.shared.infrastructure.InfrastructureService
 import com.letgo.shared.infrastructure.bus.serialize.DomainEventClassFinder
+import org.reflections.Reflections
 import kotlin.reflect.KClass
 
 @InfrastructureService
 object InMemoryDomainEventClassFinder : DomainEventClassFinder {
-    private val mapping: Map<String, KClass<out DomainEvent>> = listOf(
-        BookCreated::class,
-        BookTitleChanged::class,
-    ).associateBy { it.simpleName.toString() }
+    private var mapping: Map<String, Class<out DomainEvent>> =
+        Reflections("com.letgo.book").getSubTypesOf(DomainEvent::class.java).associateBy { it.simpleName.toString() }
 
     override fun find(type: String): KClass<out DomainEvent> =
-        mapping[type] ?: throw NoSuchElementException("No mapping for domain event with type $type")
+        mapping[type]?.kotlin ?: throw NoSuchElementException("No mapping for domain event with type $type")
 }
