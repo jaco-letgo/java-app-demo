8a71a88f5eca8e8283228211e9457492ffbfea5b
Jaco Baldrich <jaco.baldrich@letgo.com>
2022-06-29 13:24:14 +0200

    Refactor Command/Query handler finders

--- src/main/java/com/letgo/shared/infrastructure/bus/command/CommandHandlerFinder.kt
+++ src/main/java/com/letgo/shared/infrastructure/bus/command/CommandHandlerFinder.kt
@@ -3,20 +3,20 @@ package com.letgo.shared.infrastructure.bus.command
 import com.letgo.shared.application.bus.command.Command
 import com.letgo.shared.application.bus.command.CommandHandler
 import com.letgo.shared.infrastructure.InfrastructureService
-import kotlin.reflect.KClass
 
 @InfrastructureService
 class CommandHandlerFinder(handlers: List<CommandHandler<out Command>>) {
-    private val handlers =
-        handlers.associate { getCommandClass(it) to it as CommandHandler<Command> }
+    private val handlers = handlers.associate { it.commandClass() to it as CommandHandler<Command> }
 
     fun forCommand(command: Command): CommandHandler<Command> =
         handlers[command::class] ?: throw NoSuchElementException("No handler found for ${command::class}")
 
-    private fun getCommandClass(commandHandler: CommandHandler<out Command>) =
-        commandHandler::class
-            .supertypes.find { superClass -> superClass.classifier!!::class.isInstance(CommandHandler::class) }!!
-            .arguments.find { argument -> argument.type!!.classifier!!::class.isInstance(Command::class) }!!
-            .type!!
-            .classifier as KClass<out Command>
+    private fun CommandHandler<out Command>.commandClass() = this::class
+        .supertypes.firstNotNullOf { supertype ->
+            supertype.arguments.firstNotNullOf { argument ->
+                argument.type?.classifier?.takeIf {
+                    it::class.isInstance(Command::class)
+                }
+            }
+        }
 }
--- src/main/java/com/letgo/shared/infrastructure/bus/query/QueryHandlerFinder.kt
+++ src/main/java/com/letgo/shared/infrastructure/bus/query/QueryHandlerFinder.kt
@@ -4,20 +4,20 @@ import com.letgo.shared.application.bus.query.Query
 import com.letgo.shared.application.bus.query.QueryHandler
 import com.letgo.shared.application.bus.query.QueryResponse
 import com.letgo.shared.infrastructure.InfrastructureService
-import kotlin.reflect.KClass
 
 @InfrastructureService
 class QueryHandlerFinder(handlers: List<QueryHandler<out Query, out QueryResponse>>) {
-    private val handlers =
-        handlers.associate { getQueryClass(it) to it as QueryHandler<Query, QueryResponse> }
+    private val handlers = handlers.associate { it.queryClass() to it as QueryHandler<Query, QueryResponse> }
 
     fun forQuery(query: Query): QueryHandler<Query, QueryResponse> =
         handlers[query::class] ?: throw NoSuchElementException("No handler found for ${query::class}")
 
-    private fun getQueryClass(queryHandler: QueryHandler<out Query, out QueryResponse>) =
-        queryHandler::class
-            .supertypes.find { superClass -> superClass.classifier!!::class.isInstance(QueryHandler::class) }!!
-            .arguments.find { argument -> argument.type!!.classifier!!::class.isInstance(Query::class) }!!
-            .type!!
-            .classifier as KClass<out Query>
+    private fun QueryHandler<out Query, out QueryResponse>.queryClass() = this::class
+        .supertypes.firstNotNullOf { supertype ->
+            supertype.arguments.firstNotNullOf { argument ->
+                argument.type?.classifier?.takeIf {
+                    it::class.isInstance(Query::class)
+                }
+            }
+        }
 }
